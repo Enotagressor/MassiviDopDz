16aa0633ae0da2c404c3cc1a140d915d70ccabd2
Sergey <enotagressor@gmail.com>
2022-05-01 00:15:50 +0700

    4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        task3();
+        task4();
     }
 
     public static void task1() {
@@ -27,20 +28,85 @@ public class Main {
     }
 
     public static void task2() {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {5, 4, 3, 2, 1};
         System.out.println(Arrays.toString(arr));
         int[] mas = new int[arr.length];
-        for (int i = 0; i < arr.length ; i++)
-        {
-                mas[i] = arr[(arr.length - 1) - i];
-            }
-            System.out.println(Arrays.toString(mas));
+        for (int i = 0; i < arr.length; i++) {
+            mas[i] = arr[(arr.length - 1) - i];
+        }
+        System.out.println(Arrays.toString(mas));
 
     }
-    public static void task3(){
-        int[] arr = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(arr));
 
+    public static void task3() {
+        int[] arr = {5, -4, 3, 2, 1};
+        System.out.println(Arrays.toString(arr));
+         int n;
+        for (int i = 0; i < arr.length/2; i++) {
+            n = arr[i];
+            arr[i] = arr[arr.length - i -1];
+            arr[arr.length - i -1] = n;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void task4(){
+        int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int sum = -2;
+        int a = 0;
+        int b = arr.length -1;
+        while (a != b) {
+            int c = arr[a] + arr[b];
+            if (c < sum){
+                a++;
+            } else if (c>sum) {
+                b--;
+            }else {
+                System.out.println(arr[a] + " " + arr[b]);
+                break;
+            }
+        }
+    }
+//  у меня получилось 2 варианта решения задания №9,
+//  какой из них более корректен?
+    public static void task5(){
+        int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int sum = -2;
+        int a = 0;
+        int b = arr.length-1;
+        while (a<b){
+            int c = arr[a] + arr[b];
+            if (c == sum) {
+                System.out.println(arr[a] + " + " + arr[b] + " = " + c);
+                a++;
+                b--;
+            } else if(c<sum){
+                a++;
+            } else{
+                b--;
+            }
+        }
+    }
+    public static void task6(){
+       int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int summ = -2;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ( i != j && arr[i] + arr[j] == summ) {
+                if (i < j) {
+                        break;
+                    }
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + summ);
+                }
+            }
+        }
     }
 }
-
